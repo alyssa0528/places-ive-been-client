fe9f6c0c556ebe4a575da66512f4763b0292596a
alyssa0528 <alyssa.kim@gmail.com>
2018-11-23 17:49:19 -0500

    create containers folder and reorganize files, create Attractions class

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Attractions from './Attractions'
 
 class App extends Component {
   render() {
---
+++ src/containers/Attractions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Attractions extends Component {
+  render() {
+    return(
+      <div>
+        <h3>My Attractions</h3>
+      </div>
+    )
+  }
+}
+
+export default Attractions;

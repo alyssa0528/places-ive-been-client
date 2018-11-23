69b644740e1412f12cdd8ef1a35760a022192471
alyssa0528 <alyssa.kim@gmail.com>
2018-11-23 17:52:53 -0500

    create navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const navbar = (props) => {
+  return (
+    <div>
+      <h1>Places I've Been</h1>
+      <button>Home</button>
+      <button>My Places</button>
+      <button>Add a Place</button>
+    </div>
+  )
+}
+
+export default navbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Attractions from './Attractions'
+import Navbar from '../components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Places I've Been</h1>
+      <Navbar />
       </div>
     );
   }

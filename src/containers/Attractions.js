4ccc98f03e697494c2425ae495053afa5d76bb74
alyssa0528 <alyssa.kim@gmail.com>
2018-11-25 20:50:13 +0000

    add API_URL const from .env file

--- src/containers/Attractions.js
+++ src/containers/Attractions.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import AttractionList from '../components/AttractionList'
 
+const API_URL = process.env.REACT_APP_API_URL
+
 class Attractions extends Component {
   state = {
     attractions: []
   }
 
   componentDidMount() {
-    fetch('http://localhost:3001/api/attractions')
+    fetch(`${API_URL}/attractions`)
       .then(response => response.json())
       .then(attractions => this.setState({ attractions }))
   }

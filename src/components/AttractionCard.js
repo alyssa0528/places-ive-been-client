beaa15c575da2d27c1224b6d1321335fb5f61504
alyssa0528 <alyssa.kim@gmail.com>
2018-11-25 21:18:47 +0000

    refactor AttractionList and add AttractionCard

---
+++ src/components/AttractionCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AttractionCard = ({ attraction }) => (
+  <div key={attraction.id}>
+    <h3>{attraction.name}</h3>
+    <img src={attraction.img_url} alt={attraction.img_url}/>
+    <p>{attraction.city}</p>
+    <p>Year visited: {attraction.year_visited}</p>
+    <p>Thoughts: {attraction.notes}</p>
+  </div>
+)
+
+export default AttractionCard
--- src/components/AttractionList.js
+++ src/components/AttractionList.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import AttractionCard from './AttractionCard'
 
 const AttractionList = (props) => (
   <div>
-  <h2>My Places</h2>
-  {props.attractions.map(attraction =>
-    <div key={attraction.id}>
-      <h3>{attraction.name}</h3>
-      <img src={attraction.img_url} alt={attraction.img_url}/>
-      <p>{attraction.city}</p>
-      <p>Year visited: {attraction.year_visited}</p>
-      <p>Thoughts: {attraction.notes}</p>
-    </div>
-  )}
+    <h2>My Places</h2>
+    {props.attractions.map(attraction => <AttractionCard attraction={attraction} key={attraction.id}/>)}
   </div>
 )
 

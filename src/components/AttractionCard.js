ecf60944244258d3e9778333f7242ccc2c99d622
alyssa0528 <alyssa.kim@gmail.com>
2018-11-27 22:26:12 +0000

    update JSX to add city name

--- src/components/AttractionCard.js
+++ src/components/AttractionCard.js
@@ -4,10 +4,10 @@ const AttractionCard = ({ attraction }) => (
   <div key={attraction.id}>
     <h3>{attraction.name}</h3>
     <img src={attraction.img_url} alt={attraction.img_url}/>
-    <p>{attraction.city}</p>
+    <p>City: {attraction.city.name}</p>
     <p>Year visited: {attraction.year_visited}</p>
     <p>Thoughts: {attraction.notes}</p>
   </div>
 )
 
-export default AttractionCard
+export default AttractionCard;

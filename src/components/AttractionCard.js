30b510c7418236e64cc6faaabeb37560f7ea0f0d
alyssa0528 <alyssa.kim@gmail.com>
2018-11-28 14:14:05 +0000

    add preliminary CSS for AttractionCard

--- src/components/AttractionCard.js
+++ src/components/AttractionCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './AttractionCard.css'
 
 const AttractionCard = ({ attraction }) => (
-  <div key={attraction.id}>
+  <div key={attraction.id} className="AttractionCard">
     <h3>{attraction.name}</h3>
-    <img src={attraction.img_url} alt={attraction.img_url}/>
+    <img src={attraction.img_url} alt={attraction.img_url} className="AttractionImage"/>
     <p>City: {attraction.city.name}</p>
     <p>Year visited: {attraction.year_visited}</p>
     <p>Thoughts: {attraction.notes}</p>

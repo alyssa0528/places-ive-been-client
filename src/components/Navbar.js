f0dc1a4e1ee20f71335b73e5490a62b5fb9f05bb
alyssa0528 <alyssa.kim@gmail.com>
2018-11-30 15:50:10 +0000

    add Google font and minimal styling to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
+import '../index.css'
 
 const navbar = (props) => {
   return (
     <div>
-      <h1>Places I've Been</h1>
-      <Link to='/'>Home</Link>
-      <Link to='/places'>My Places</Link>
-      <Link to='/places/new'>Add a Place</Link>
+      <h1 className="appHeader">Places I've Been</h1>
+      <Link to='/' className="Navbar">Home</Link>
+      <Link to='/places' className="Navbar">My Places</Link>
+      <Link to='/places/new' className="Navbar">Add a Place</Link>
     </div>
   )
 }

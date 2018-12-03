d5fa7fe6619e8d401f826109cce0349720a900f9
alyssa0528 <alyssa.kim@gmail.com>
2018-12-03 10:09:02 +0000

    add maxWidth to image

--- src/components/Attraction.js
+++ src/components/Attraction.js
@@ -1,14 +1,12 @@
 //this file is for presenting a single attraction at /attractions/:id
 import React from 'react';
-import './Attraction.css';
 import { Link } from 'react-router-dom'
-import DeleteButton from './DeleteButton'
 
 const attraction = ({ attraction }) => {
   return(
     <div>
       <h3>{attraction.name}</h3>
-      <img src={attraction.img_url} alt={attraction.img_url} className="AttractionImage"/>
+      <img src={attraction.img_url} alt={attraction.img_url} style={{maxWidth: 600}}/>
       <p>City: {attraction.city}</p>
       <p>Year visited: {attraction.year_visited}</p>
       <p>Thoughts: {attraction.notes}</p>

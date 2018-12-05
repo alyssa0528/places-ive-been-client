839308f9ea75d7c65c279886b02de77a477ab47a
alyssa0528 <alyssa.kim@gmail.com>
2018-12-05 11:39:51 +0000

    add AttractionList to import, remove Attractions from import

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.css';
-import Attractions from './attractions/Attractions';
 import AttractionPhotoList from './attractions/AttractionPhotoList';
 import AttractionFormContainer from './attractionForm/AttractionFormContainer';
 import AttractionContainer from './attraction/AttractionContainer';
+import AttractionList from './attractions/AttractionList';
 import Navbar from '../components/Navbar';
 
 class App extends Component {
@@ -17,7 +17,7 @@ class App extends Component {
             <Navbar />
               <Switch>
                 <Route exact path="/" component={AttractionPhotoList} />
-                <Route exact path="/places" component={Attractions} />
+                <Route exact path="/places" component={AttractionList} />
                 <Route exact path="/places/new" component={AttractionFormContainer} />
                 <Route path="/places/:id" component={AttractionContainer} />
               </Switch>
